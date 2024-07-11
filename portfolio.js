6138de2f06c5f136ab2c072bb6f75d25c96a14b1
kareem sheik <137775607+kareemsheik@users.noreply.github.com>
2024-07-11 22:11:59 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+function facebook(){
+    const button = document.getElementById("fb");
+  
+    button.addEventListener("click", function() {
+      window.location.href = "https://www.facebook.com"; // Replace with your desired website URL
+    });
+}
